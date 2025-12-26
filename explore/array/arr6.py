0e80e1ac85b1fc21f62d93e3eeaf70734010928a
tjy <2578689251@qq.com>
2025-12-26 14:40:02 +0800

    数组练习2

--- explore/array/arr6.py
+++ explore/array/arr6.py
@@ -18,3 +18,7 @@ def findDisappearedNumbers(nums: List[int]) -> List[int]:
 if __name__ == '__main__':
     nums = [4,3,2,7,8,2,3,1]
     print(findDisappearedNumbers(nums))
+
+"""
+列表不能直接相减，可以转换为set集合
+"""
